af1a437de1ec287307d0c581861962199a80e74d
heripatel09496-prog <heripatel09496@gmail.com>
2026-08-06 12:53:06 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,1 +1,10 @@
+n = int(input("Enter number of elements: "))
 
+a = list(map(int, input("Enter numbers: ").split()))
+
+for i in range(len(a) - 1):
+
+    if a[i] == a[i+1]:
+      
+        if i == 0 or a[i] != a[i-1]:
+            print(a[i])
